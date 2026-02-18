e393bf17ba38f063457d65fb8589df95ed0838d0
romarica-droid <romarica@uci.edu>
2026-02-17 18:26:28 -0800

    Feat: Start of player movement

---
+++ GDIM32_Final/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _player;
+
+    private float _speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horiztonal = Input.GetAxis("Horizontal");
+
+        transform.Translate(((vertical * Vector3.forward) + (horiztonal * Vector3.right)) * _speed * Time.deltaTime);
+    }
+}
